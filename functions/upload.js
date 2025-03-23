e017c7dd43af5868a0975aa45dc97684c7e33d8e
Michal Kolodziejczyk <koczol123@gmail.com>
2025-03-23 18:51:13 +0100

    Opis zmian

--- functions/upload.js
+++ functions/upload.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 exports.handler = async function(event, context) {
     console.log("Funkcja została wywołana");
-
+    
     if (event.httpMethod !== 'POST') {
         console.log("Nieprawidłowa metoda HTTP");
         return { statusCode: 405, body: 'Method Not Allowed' };
@@ -17,66 +17,40 @@ exports.handler = async function(event, context) {
             uploadDir: tempDir,
             keepExtensions: true,
             multiples: true,
-            maxFileSize: 50 * 1024 * 1024, // Limity na pliki - do 50MB
         });
+
+        // Zamiana body eventu na Buffer
+        const formData = Buffer.from(event.body, 'base64');
 
         // Przetwarzanie formularza
         const { fields, files } = await new Promise((resolve, reject) => {
-            const fakeReq = {
-                headers: event.headers,
-                body: Buffer.from(event.body, 'base64') // Zmieniamy ciało na Buffer
-            };
-
-            form.parse(fakeReq, (err, fields, files) => {
-                if (err) reject(err);
-                resolve({ fields, files });
+            form.parse({ headers: event.headers, body: formData }, (err, fields, files) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ fields, files });
+                }
             });
         });
-
-        console.log("Formularz przetworzony", fields, files);
 
         const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
         console.log("Dropbox zainicjowany");
 
-        const guestName = fields['guest-name'] ? fields['guest-name'].replace(/ /g, '_') : 'unknown_guest';
-        const guestEmail = fields['guest-email'] || 'brak_emaila';
+        const guestName = fields['guest-name'].replace(/ /g, '_');
+        const guestEmail = fields['guest-email'];
 
         // Przesyłanie plików do Dropbox
         const uploadPromises = Object.values(files).map(async (fileObj) => {
             const fileName = `${guestName}_${fileObj.originalFilename}`;
             console.log("Przesyłanie pliku:", fileName);
 
-            try {
-                const fileContent = fs.readFileSync(fileObj.filepath);
-                const response = await dbx.filesUpload({
-                    path: `/aplikacje/wesele_kasia_michal/${fileName}`,
-                    contents: fileContent,
-                });
+            const fileContent = fs.readFileSync(fileObj.filepath);
 
-                console.log("Plik przesłany do Dropbox:", fileName, response);
-                
-                // Usunięcie tymczasowego pliku
-                fs.unlinkSync(fileObj.filepath);
-            } catch (err) {
-                console.error("Błąd przy przesyłaniu pliku:", fileName, err);
-                throw err; // Rzucenie błędu zatrzyma dalsze przesyłanie
-            }
-        });
+            await dbx.filesUpload({
+                path: `/aplikacje/wesele_kasia_michal/${fileName}`,
+                contents: fileContent
+            });
 
-        // Czekamy na przesłanie wszystkich plików
-        await Promise.all(uploadPromises);
-        console.log("Wszystkie pliki przesłane pomyślnie");
-
-        return {
-            statusCode: 200,
-            body: 'Zdjęcia przesłane! Dziękujemy!'
-        };
-
-    } catch (err) {
-        console.error("Błąd podczas przesyłania plików:", err);
-        return {
-            statusCode: 500,
-            body: 'Błąd przy przesyłaniu zdjęć: ' + err.message
-        };
-    }
-};
+            // Usunięcie tymczasowego pliku
+            fs.unlinkSync(fileObj.filepath);
+            console.log("Plik przesłany:", fileName
