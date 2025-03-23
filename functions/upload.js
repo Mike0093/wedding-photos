4ce1179dd89520eb3d150be0f71c0e27637b3811
Michal Kolodziejczyk <koczol123@gmail.com>
2025-03-23 18:56:12 +0100

    Opis zmian

--- functions/upload.js
+++ functions/upload.js
@@ -9,7 +9,7 @@ exports.handler = async function(event, context) {
         console.log("Nieprawidłowa metoda HTTP");
         return { statusCode: 405, body: 'Method Not Allowed' };
     }
-
+    
     try {
         // Utworzenie tymczasowego pliku ze strumienia danych
         const tempDir = '/tmp'; // Netlify Functions pozwala na używanie /tmp
@@ -18,47 +18,46 @@ exports.handler = async function(event, context) {
             keepExtensions: true,
             multiples: true,
         });
-
-        // Zamiana body eventu na Buffer
-        const formData = Buffer.from(event.body, 'base64');
-
+        
         // Przetwarzanie formularza
         const { fields, files } = await new Promise((resolve, reject) => {
-            form.parse({ headers: event.headers, body: formData }, (err, fields, files) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve({ fields, files });
-                }
+            const fakeReq = {
+                headers: event.headers,
+                body: Buffer.from(event.body, 'base64')
+            };
+            
+            form.parse(fakeReq, (err, fields, files) => {
+                if (err) reject(err);
+                resolve({ fields, files });
             });
         });
-
+        
         const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
         console.log("Dropbox zainicjowany");
-
+        
         const guestName = fields['guest-name'].replace(/ /g, '_');
         const guestEmail = fields['guest-email'];
-
+        
         // Przesyłanie plików do Dropbox
         const uploadPromises = Object.values(files).map(async (fileObj) => {
             const fileName = `${guestName}_${fileObj.originalFilename}`;
             console.log("Przesyłanie pliku:", fileName);
-
+            
             const fileContent = fs.readFileSync(fileObj.filepath);
-
+            
             await dbx.filesUpload({
                 path: `/aplikacje/wesele_kasia_michal/${fileName}`,
                 contents: fileContent
             });
-
+            
             // Usunięcie tymczasowego pliku
             fs.unlinkSync(fileObj.filepath);
             console.log("Plik przesłany:", fileName);
         });
-
+        
         await Promise.all(uploadPromises);
         console.log("Wszystkie pliki przesłane pomyślnie");
-
+        
         return {
             statusCode: 200,
             body: 'Zdjęcia przesłane! Dziękujemy!'
