025ee93421cf6d2a9b260383726b5d18f9e4d553
Michal Kolodziejczyk <koczol123@gmail.com>
2025-03-23 19:10:40 +0100

    Opis zmian

--- functions/upload.js
+++ functions/upload.js
@@ -1,79 +1,78 @@
 const { Dropbox } = require('dropbox');
 const formidable = require('formidable');
 const fs = require('fs');
-const { Buffer } = require('buffer');
+const streamifier = require('streamifier');
 
 exports.handler = async function(event, context) {
-    console.log("Funkcja została wywołana");
+  console.log("Funkcja została wywołana");
 
-    if (event.httpMethod !== 'POST') {
-        console.log("Nieprawidłowa metoda HTTP");
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
+  if (event.httpMethod !== 'POST') {
+    console.log("Nieprawidłowa metoda HTTP");
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
-    try {
-        // Utworzenie tymczasowego pliku ze strumienia danych
-        const tempDir = '/tmp'; // Netlify Functions pozwala na używanie /tmp
-        const form = new formidable.IncomingForm({
-            uploadDir: tempDir,
-            keepExtensions: true,
-            multiples: true,
-        });
+  try {
+    const tempDir = '/tmp'; // Netlify Functions pozwala na używanie /tmp
+    const form = new formidable.IncomingForm({
+      uploadDir: tempDir,
+      keepExtensions: true,
+      multiples: true,
+    });
 
-        // Przekształcenie base64 na buffer, aby móc wykorzystać form.parse()
-        const bodyBuffer = Buffer.from(event.body, 'base64');
-        const fakeReq = {
-            headers: event.headers,
-            body: bodyBuffer,
-        };
+    // Konwersja event.body z base64 na Buffer
+    const bodyBuffer = Buffer.from(event.body, 'base64');
 
-        // Przetwarzanie formularza
-        const { fields, files } = await new Promise((resolve, reject) => {
-            form.parse(fakeReq, (err, fields, files) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve({ fields, files });
-                }
-            });
-        });
+    // Konwersja bufora na strumień przy użyciu streamifier
+    const reqStream = streamifier.createReadStream(bodyBuffer);
+    // Dodanie nagłówków do strumienia (wymagane przez formidable)
+    reqStream.headers = event.headers;
 
-        // Inicjalizacja Dropbox
-        const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
-        console.log("Dropbox zainicjowany");
+    // Parsowanie danych formularza
+    const { fields, files } = await new Promise((resolve, reject) => {
+      form.parse(reqStream, (err, fields, files) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve({ fields, files });
+      });
+    });
 
-        const guestName = fields['guest-name'].replace(/ /g, '_');
-        const guestEmail = fields['guest-email'];
+    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
+    console.log("Dropbox zainicjowany");
 
-        // Przesyłanie plików do Dropbox
-        const uploadPromises = Object.values(files).map(async (fileObj) => {
-            const fileName = `${guestName}_${fileObj.originalFilename}`;
-            console.log("Przesyłanie pliku:", fileName);
+    const guestName = fields['guest-name'].replace(/ /g, '_');
+    // guestEmail jest tu nieużywany, ale można go ewentualnie wykorzystać
+    const guestEmail = fields['guest-email'];
 
-            const fileContent = fs.readFileSync(fileObj.filepath);
+    // Przesyłanie plików do Dropbox
+    const uploadPromises = Object.values(files).map(async (fileObj) => {
+      const fileName = `${guestName}_${fileObj.originalFilename}`;
+      console.log("Przesyłanie pliku:", fileName);
 
-            await dbx.filesUpload({
-                path: `/aplikacje/wesele_kasia_michal/${fileName}`,
-                contents: fileContent
-            });
+      const fileContent = fs.readFileSync(fileObj.filepath);
 
-            // Usunięcie tymczasowego pliku
-            fs.unlinkSync(fileObj.filepath);
-            console.log("Plik przesłany:", fileName);
-        });
+      await dbx.filesUpload({
+        path: `/aplikacje/wesele_kasia_michal/${fileName}`,
+        contents: fileContent
+      });
 
-        await Promise.all(uploadPromises);
-        console.log("Wszystkie pliki przesłane pomyślnie");
+      // Usunięcie tymczasowego pliku
+      fs.unlinkSync(fileObj.filepath);
+      console.log("Plik przesłany:", fileName);
+    });
 
-        return {
-            statusCode: 200,
-            body: 'Zdjęcia przesłane! Dziękujemy!'
-        };
-    } catch (err) {
-        console.error("Błąd podczas przesyłania plików:", err);
-        return {
-            statusCode: 500,
-            body: 'Błąd przy przesyłaniu zdjęć: ' + err.message
-        };
-    }
+    await Promise.all(uploadPromises);
+    console.log("Wszystkie pliki przesłane pomyślnie");
+
+    return {
+      statusCode: 200,
+      body: 'Zdjęcia przesłane! Dziękujemy!'
+    };
+  } catch (err) {
+    console.error("Błąd podczas przesyłania plików:", err);
+    return {
+      statusCode: 500,
+      body: 'Błąd przy przesyłaniu zdjęć: ' + err.message
+    };
+  }
 };
